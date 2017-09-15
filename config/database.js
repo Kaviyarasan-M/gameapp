31e30f1b10e4938fa1c38c58227040d78663f57c
selva <selvarudram@gmail.com>
2017-09-15 14:14:55 +0530

    Initial Commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
 	'secret': 'Strictly',
-	  'database':'mongodb://localhost:27017/gameapp'
-	  
+	 // 'database':'mongodb://localhost:27017/gameapp'
+	'database' : 'mongodb://kavi:Strictly.mlab.com:35594/gameapp'
+
+	
 }
