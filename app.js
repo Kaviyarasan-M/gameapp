9b21d93b2e862634ba6c0edc4262c3af14f06948
selva <selvarudram@gmail.com>
2017-09-27 15:21:02 +0530

    api added

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-//var config = require('./config/config');
+var authUser = require('./app/services/authService');
+var config = require('./config/config');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
