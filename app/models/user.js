da85a72d58cfa5a59c35dc3906255d6cd419f1c7
selva <selvarudram@gmail.com>
2017-09-27 15:50:05 +0530

    api added

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 //set up a mongoose model
 module.exports = mongoose.model('User', new Schema({
 
-      id: String,
+      id: Number,
       user_name: String,
       full_name: String,
       bio: String,
