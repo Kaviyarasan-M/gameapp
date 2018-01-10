4604e62a7874aef106c91437ba9ce2569c5757a4
selva <selvarudram@gmail.com>
2018-01-10 10:30:48 +0530

    api modified

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 
 
 
-var in_client_id = '5ee0174a2eb840f88cee2f37ab75d02b',
-    in_client_secret = '79a26b125d0843f1baa21e2a9e82afee',
+var in_client_id = '3798d1b023474acc8caa2051aaf21457',
+    in_client_secret = '548fb12dd1cd4133bd662be7aa4e0460',
     in_redirect_uri = 'https://glacial-garden-26289.herokuapp.com/api/auth',
     in_auth_url = 'https://api.instagram.com/oauth/authorize/?client_id='
                   + in_client_id + '&redirect_uri='
@@ -23,3 +23,9 @@ module.exports = {
 };
 
 
+
+//https://api.instagram.com/oauth/authorize/?client_id=5ee0174a2eb840f88cee2f37ab75d02b&redirect_uri=https://glacial-garden-26289.herokuapp.com/api/auth&response_type=code
+
+
+    //in_client_id = '5ee0174a2eb840f88cee2f37ab75d02b',
+    //in_client_secret = '79a26b125d0843f1baa21e2a9e82afee',
